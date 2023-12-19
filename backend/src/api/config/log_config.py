a32db5a866949a5e86ffe4e538e4883a39b89ad0
Dale-Kurt Murray <hello@dalekurtmurray.com>
2023-12-19 16:47:26 -0500

    Updated logger

--- backend/src/api/config/log_config.py
+++ backend/src/api/config/log_config.py
@@ -8,6 +8,7 @@ from loguru import logger
 def configure_logging():
     # Configure logging to write to both console and a log file
     logger.add(sys.stdout, level="INFO")
-    logger.add("logs/app.log", rotation="5 MB", level="INFO")
+    # logger.add("logs/app.log", rotation="5 MB", level="INFO")
+    logger.add("logs/app_{time:YYYY-MM-DD}.log", rotation="5 MB", level="INFO")
 
     logger.info("Logging configured successfully")
